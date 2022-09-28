beba2c63bf31f61538c15ed3c56a251fd29fa28a
zzzzer <zzzzer91@gmail.com>
2022-09-28 15:42:31 +0800

    optimize: 优化trace

--- gintrace/trace.go
+++ gintrace/trace.go
@@ -12,13 +12,14 @@ func Trace() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, span := tracex.StartTracing(newCtx(c), c.Request.Method+" "+c.FullPath())
 		defer span.End()
-		span.SetAttributes(attribute.String("uri", c.Request.RequestURI))
+		span.SetAttributes(attribute.String("path", c.Request.URL.Path+"?"+c.Request.URL.RawQuery))
 		span.SetAttributes(attribute.String("method", c.Request.Method))
 		span.SetAttributes(attribute.String("ip", c.ClientIP()))
 		span.SetAttributes(attribute.String("userAgent", c.Request.UserAgent()))
-		span.SetAttributes(attribute.Int("contentLength", int(c.Request.ContentLength)))
+		span.SetAttributes(attribute.Int("req.body.size", int(c.Request.ContentLength)))
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
+		span.SetAttributes(attribute.Int("resp.body.size", c.Writer.Size()))
 	}
 }
 
