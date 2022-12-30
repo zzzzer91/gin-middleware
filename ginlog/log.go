fc689c553d65b9977e6371fc05702c3e9dd37860
zzzzer <zzzzer91@gmail.com>
2022-12-30 18:35:57 +0800

    feat: update ginlog

--- ginlog/log.go
+++ ginlog/log.go
@@ -17,10 +17,14 @@ const (
 func Log(isLogInfo bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
-		c.Request = c.Request.WithContext(newCtx(c))
+		ctx := newCtx(c)
+		if isLogInfo {
+			zlog.Ctx(ctx).Info(buildBeginMsg(c))
+		}
+		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 		used := time.Since(start)
-		ctx := c.Request.Context()
+		ctx = c.Request.Context()
 		var err error
 		if len(c.Errors) > 0 {
 			err = c.Errors[len(c.Errors)-1].Err
@@ -29,7 +33,7 @@ func Log(isLogInfo bool) gin.HandlerFunc {
 			zlog.Ctx(ctx).WithError(err).Error(buildErrorMsg(c, used))
 		} else {
 			if isLogInfo {
-				zlog.Ctx(ctx).Info(buildInfoMsg(c, used))
+				zlog.Ctx(ctx).Info(buildCostMsg(c, used))
 			}
 		}
 	}
@@ -45,7 +49,7 @@ func newCtx(c *gin.Context) context.Context {
 	return ctx
 }
 
-func buildInfoMsg(c *gin.Context, used time.Duration) string {
+func buildBasicMsg(c *gin.Context) strings.Builder {
 	sb := strings.Builder{}
 	sb.WriteString(c.ClientIP())
 	sb.WriteByte(' ')
@@ -56,22 +60,24 @@ func buildInfoMsg(c *gin.Context, used time.Duration) string {
 		sb.WriteByte('?')
 		sb.WriteString(c.Request.URL.RawQuery)
 	}
+	return sb
+}
+
+func buildBeginMsg(c *gin.Context) string {
+	sb := buildBasicMsg(c)
+	sb.WriteString("`, started")
+	return sb.String()
+}
+
+func buildCostMsg(c *gin.Context, used time.Duration) string {
+	sb := buildBasicMsg(c)
 	sb.WriteString("`, used ")
 	sb.WriteString(used.String())
 	return sb.String()
 }
 
 func buildErrorMsg(c *gin.Context, used time.Duration) string {
-	sb := strings.Builder{}
-	sb.WriteString(c.ClientIP())
-	sb.WriteByte(' ')
-	sb.WriteString(c.Request.Method)
-	sb.WriteString(" `")
-	sb.WriteString(c.Request.URL.Path)
-	if len(c.Request.URL.RawQuery) > 0 {
-		sb.WriteByte('?')
-		sb.WriteString(c.Request.URL.RawQuery)
-	}
+	sb := buildBasicMsg(c)
 	sb.WriteString("`, used ")
 	sb.WriteString(used.String())
 	sb.WriteString(", error!")
