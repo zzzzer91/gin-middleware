3b7539cfc34429d493eba687cd9255c2f638fed7
zzzzer <zzzzer91@gmail.com>
2023-05-05 13:14:04 +0800

    feat: update gintrace

--- gintrace/trace.go
+++ gintrace/trace.go
@@ -9,18 +9,22 @@ import (
 func Trace() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, span := trace.StartTracing(c.Request.Context(), c.Request.Method+" "+c.FullPath())
-		defer span.End()
-		path := c.Request.URL.Path
-		if len(c.Request.URL.RawQuery) > 0 {
-			path += "?" + c.Request.URL.RawQuery
+		if span.IsRecording() {
+			defer span.End()
+			path := c.Request.URL.Path
+			if len(c.Request.URL.RawQuery) > 0 {
+				path += "?" + c.Request.URL.RawQuery
+			}
+			span.SetAttributes(attribute.String("path", path))
+			span.SetAttributes(attribute.String("method", c.Request.Method))
+			span.SetAttributes(attribute.String("ip", c.ClientIP()))
+			span.SetAttributes(attribute.String("userAgent", c.Request.UserAgent()))
+			span.SetAttributes(attribute.Int("req.body.size", int(c.Request.ContentLength)))
+			c.Request = c.Request.WithContext(ctx)
+			c.Next()
+			span.SetAttributes(attribute.Int("resp.body.size", c.Writer.Size()))
+		} else {
+			c.Next()
 		}
-		span.SetAttributes(attribute.String("path", path))
-		span.SetAttributes(attribute.String("method", c.Request.Method))
-		span.SetAttributes(attribute.String("ip", c.ClientIP()))
-		span.SetAttributes(attribute.String("userAgent", c.Request.UserAgent()))
-		span.SetAttributes(attribute.Int("req.body.size", int(c.Request.ContentLength)))
-		c.Request = c.Request.WithContext(ctx)
-		c.Next()
-		span.SetAttributes(attribute.Int("resp.body.size", c.Writer.Size()))
 	}
 }
